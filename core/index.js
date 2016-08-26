9e2ac233497972d62a8188d157acebdb8e4a6a76
Vlad Filip <vlad.filip@williamhill.com>
2016-08-26 17:52:45 +0200

    converting app to a one-trick-pony

--- core/index.js
+++ core/index.js
@@ -1,33 +1,76 @@
 'use strict';
 
 var utils = require( __dirname + '/utils' );
+var http = require( 'http' );
+var https = require( 'https' );
 
-function Apigeon( app, server, paths ) {
+function Apigeon( options ) {
 
-    // Extend app with websocket potential
-    require( 'express-ws' )( app, server );
+    var config = utils.merge( {}, {
+        paths: {
+            apis: null,
+            drivers: null,
+            renderers: null
+        },
+        httpsOptions: null
+    }, options );
 
-    this.paths = utils.merge( {}, {
-        apis: __dirname + '/../../../api',
-        renderers: null,
-        drivers: null
-    }, paths );
+    var server = null;
+
+    if ( !config.httpsOptions ) {
+        server = http.createServer();
+    } else {
+        server = https.createServer( config.httpsOptions );
+    }
+
+    // Register all connections
+    var connections = {};
+    server.on( 'connection', function ( socket ) {
+        var id = ( new Date() ).getTime() + '' + Math.floor( Math.random() * 10000000 ); // Unique Id
+        connections[ id ] = socket;
+        socket.on( 'close', function () {
+            delete connections[ id ];
+        } );
+    } );
+
+    var startServer = function ( port, done ) {
+        server.listen( port, done );
+    };
+
+    var stopServer = function ( done ) {
+        if ( server.listening ) {
+            for ( var i in connections ) {
+                if ( connections.hasOwnProperty( i ) ) {
+                    connections[ i ].destroy();
+                }
+            }
+            server.close( done );
+        } else {
+            done();
+        }
+    };
+
+    this.attach = function ( serverType ) {
+        if ( typeof serverType === 'function' ) {
+            serverType( server );
+        }
+    };
+
+    this.start = startServer;
+    this.stop = stopServer;
+
+    Apigeon.prototype.REST = require( __dirname + '/webserver/rest-middleware' )( config.paths );
+    Apigeon.prototype.WS = require( __dirname + '/webserver/ws-middleware' )( config.paths );
 
 }
 
-Apigeon.prototype.session = function ( config ) {
-    var drivers = this.paths.drivers;
-    return require( __dirname + '/webserver/session-middleware' )( drivers, config );
-};
-Apigeon.prototype.logs = function ( config ) {
-    var drivers = this.paths.drivers;
-    return require( __dirname + '/webserver/logs-middleware' )( drivers, config );
-};
-Apigeon.prototype.rest = function () {
-    return require( __dirname + '/webserver/rest-middleware' )( this.paths );
-};
-Apigeon.prototype.ws = function () {
-    return require( __dirname + '/webserver/ws-middleware' )( this.paths );
-};
+// Apigeon.prototype.session = function ( config ) {
+//     var drivers = this.paths.drivers;
+//     return require( __dirname + '/webserver/session-middleware' )( drivers, config );
+// };
+// Apigeon.prototype.logs = function ( config ) {
+//     var drivers = this.paths.drivers;
+//     return require( __dirname + '/webserver/logs-middleware' )( drivers, config );
+// };
 
 module.exports = Apigeon;
--- core/webserver/ws-middleware.js
+++ core/webserver/ws-middleware.js
@@ -2,27 +2,65 @@
 
 var WebSocketServer = require( 'ws' ).Server;
 
-module.exports = function ( server, paths ) {
+var url = require( 'url' );
 
-    var ws = new WebSocketServer( {
-        server: server
-    } );
+var ErrorClass = require( __dirname + '/../libs/errorClass' );
+var RendererClass = require( __dirname + '/../libs/rendererClass' );
+var loadApi = require( __dirname + '/../libs/loadApi' );
 
-    ws.on( 'connection', function connection( socket ) {
-        var location = url.parse( socket.upgradeReq.url, true );
+module.exports = function ( paths ) {
 
-        // you might use location.query.access_token to authenticate or share sessions
-        // or ws.upgradeReq.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
+    return function ( server ) {
 
-        socket.on( 'message', function incoming( message ) {
-            console.log( 'received: %s', message );
+        var ws = new WebSocketServer( {
+            server: server
         } );
 
-        socket.send( 'something' );
-    } );
+        ws.on( 'connection', function ( socket ) {
+            var req = socket.upgradeReq;
+            var location = url.parse( req.url, true );
 
-    return function ( req, res ) {
+            var api = loadApi( paths.apis, location.pathname, req );
+            var renderer = new RendererClass( paths.renderers, api );
+
+            var error = false;
+            if ( !api ) {
+                error = new ErrorClass( 404 );
+            } else {
+                if ( !api.methodAllowed( 'SOCKET' ) ) {
+                    error = new ErrorClass( 405 );
+                }
+                if ( !api.protocolAllowed( req.protocol ) ) {
+                    error = new ErrorClass( 403 );
+                }
+            }
+            if ( error ) {
+                socket.send( renderer.render( error.getMessage() ) );
+                socket.close();
+                return;
+            }
+
+            socket.on( 'message', function incoming( message ) {
+                console.log( 'received: %s', message );
+            } );
+
+            socket.send( 'something' );
+        } );
 
 
+
+
+        socket.on( 'message', function ( message ) {
+            req.body = message;
+            api._getData( function ( data ) {
+                socket.send( renderer.render( data ) );
+            }, function ( error ) {
+                socket.send( renderer.render( error.getMessage() ) );
+            } );
+        } );
+
+        socket.on( 'close', function () {
+            api.terminate();
+        } );
     };
 };
