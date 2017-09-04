b498f50681f7922adc24fb9ff349b189e804e04d
Vlad <vlad.filip.dev@gmail.com>
2017-09-04 16:30:33 +0300

    removed unneeded status in connection class

--- core/libs/ConnectionClass.js
+++ core/libs/ConnectionClass.js
@@ -6,13 +6,9 @@ class ConnectionClass {
 
   constructor ( socket ) {
     this.id = utils.uid()
-    this.status = 1
     socket.apigeon = socket.apigeon || {}
     socket.apigeon.connectionId = this.id
     this.socket = socket
-    socket.on( 'close', () => {
-      this.status = 0
-    } )
   }
 
   close () {
